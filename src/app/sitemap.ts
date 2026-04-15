6aae2f325b7b11efd8422f21f12c8bf4195a0745
Serhii Vladimirov <vladimirovsk@gmail.com>
2026-04-15 06:13:07 -0600

    added sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = "https://wildlink.ca";
   const lastModified = new Date("2025-04-01");
